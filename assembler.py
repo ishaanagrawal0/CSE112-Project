075fb73eba00222094b9e8a5e03b2e9d6080034a
Ishaan Agrawal <113104833+ishaanagrawal0@users.noreply.github.com>
2023-04-21 19:53:17 +0530

    Added opcodes for operations

--- assembler.py
+++ assembler.py
@@ -4,3 +4,24 @@ Debjit Banerji 2022146
 Himang Chandra Garg 2022214
 Ishaan Agrawal 2022221
 '''
+
+operations = {
+    'add' : '00000',
+    'sub' : '00001',
+    'movI' : '00010', #Need changes here
+    'movR' : '00011',
+    'ld' : '00100',
+    'st' : '00101',
+    'mul' : '00110',
+    'div' : '00111',
+    'rs' : '01000',
+    'ls' : '01001',
+    'xor' : '01010',
+    'and' : '01011',
+    'cmp' : '01110',
+    'jmp' : '01111',
+    'jlt' : '11100',
+    'jgt' : '11101',
+    'je' : '11111',
+    'hlt' : '11010',
+}

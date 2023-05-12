62d70252303702f01ba256e90317a75ac0b3d9b2
Ishaan Agrawal <113104833+ishaanagrawal0@users.noreply.github.com>
2023-05-12 22:36:57 +0530

    Deleted a redudant dictionary

--- assembler.py
+++ assembler.py
@@ -6,28 +6,7 @@ Ishaan Agrawal 2022221
 '''
 
 dictionary_of_variables={} #dictionary to store the variables
-operations = {
-    'add' : '00000',
-    'sub' : '00001',
-    'movI' : '00010', #Need changes here
-    'movR' : '00011',
-    'ld' : '00100',
-    'st' : '00101',
-    'mul' : '00110',
-    'div' : '00111',
-    'rs' : '01000',
-    'ls' : '01001',
-    'xor' : '01010',
-    'or': '01011',
-    'and' : '01100',
-    'not': '01101',
-    'cmp' : '01110',
-    'jmp' : '01111',
-    'jlt' : '11100',
-    'jgt' : '11101',
-    'je' : '11111',
-    'hlt' : '11010',
-}
+
 #7 registers and one flag register we have in the question
 registers={
     "R0": "000",
